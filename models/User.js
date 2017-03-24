f1eafd04e1c7175257fb479129f92aa6c143f2a3
Martin John R. Lopez <Tinjo@Martins-MacBook-Pro.local>
2017-03-24 17:08:11 +0800

    add fancybox gallery

--- models/User.js
+++ models/User.js
@@ -10,6 +10,7 @@ var User = new keystone.List('User');
 User.add({
 	name: { type: Types.Name, required: true, index: true },
 	email: { type: Types.Email, initial: true, required: true, unique: true, index: true },
+	photo: { type: Types.CloudinaryImage, collapse: true },
 	password: { type: Types.Password, initial: true, required: true },
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
