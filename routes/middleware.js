a131ec145c3c1ac662f98224f45752fed0eb9833
Martin John R. Lopez <Tinjo@Martins-MacBook-Pro.local>
2017-03-30 09:09:27 +0800

    remove gallery from navlinks

--- routes/middleware.js
+++ routes/middleware.js
@@ -21,7 +21,6 @@ exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
 		{ label: 'Home', key: 'home', href: '/' },
 		{ label: 'Notes', key: 'blog', href: '/blog' },
-		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
 		{ label: 'Contact', key: 'contact', href: '/contact' },
 	];
 	res.locals.user = req.user;
